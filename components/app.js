a79b787f44893c2227ecd92f6b5ccadde1308f8a
Avishekpanta <98971470+Avishekpanta@users.noreply.github.com>
2023-08-10 10:47:08 +0545

    Initial commit

---
+++ components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header.js";
+import Footer from "./footer";
+import Note from "./note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
